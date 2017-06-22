3fe2cc11e61ceae1354e0603b34293d56f9ab5a3
None <none@example.com>
2017-06-22 16:39:42 -0400

    Typo

--- config/scripts/post-config.d/hooks.sh
+++ config/scripts/post-config.d/hooks.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-source /configu/user-data/edgerouter-backup.conf
+source /config/user-data/edgerouter-backup.conf
 
 sudo ln -fs /config/user-data/hooks/* /etc/commit/post-hooks.d/
 exit 0
