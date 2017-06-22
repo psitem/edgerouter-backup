4b0b21638735d25cfbb2c78e733af2578b815d8f
None <none@example.com>
2017-06-22 16:41:13 -0400

    Sudo all the things.

--- config/scripts/post-config.d/hooks.sh
+++ config/scripts/post-config.d/hooks.sh
@@ -2,4 +2,5 @@
 source /config/user-data/edgerouter-backup.conf
 
 sudo ln -fs /config/user-data/hooks/* /etc/commit/post-hooks.d/
+sudo chmod +x /config/user-data/hooks/*
 exit 0
--- config/user-data/hooks/03commands.sh
+++ config/user-data/hooks/03commands.sh
@@ -12,9 +12,9 @@ sudo chmod 664 /config/user-data/$HOSTNAME.commands.conf
 
 sudo cli-shell-api showConfig --show-active-only > /config/user-data/$HOSTNAME.config.conf
 sudo cli-shell-api showConfig --show-commands --show-active-only > /config/user-data/$HOSTNAME.commands.conf
-scp -i $SSH_KEYFILE -o StrictHostKeyChecking=no /config/user-data/$HOSTNAME.*.conf $SSH_USER@$SSH_HOST:$REPO_PATH/
+sudo scp -i $SSH_KEYFILE -o StrictHostKeyChecking=no /config/user-data/$HOSTNAME.*.conf $SSH_USER@$SSH_HOST:$REPO_PATH/
 
-ssh -i $SSH_KEYFILE -o StrictHostKeyChecking=no $SSH_USER@$SSH_HOST 'bash -s' << ENDSSH
+sudo ssh -i $SSH_KEYFILE -o StrictHostKeyChecking=no $SSH_USER@$SSH_HOST 'bash -s' << ENDSSH
 cd $REPO_PATH
 git add --all
 git commit -m "Auto-commit"
