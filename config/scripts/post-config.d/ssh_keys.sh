e03aba85796d8d6973db13fad744e204c41a833f
None <none@example.com>
2017-06-22 16:57:54 -0400

    Fixes

--- config/scripts/post-config.d/ssh_keys.sh
+++ config/scripts/post-config.d/ssh_keys.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Set ownership and permissions for ubnt user
-source /configu/user-data/edgerouter-backup.conf
+source /config/user-data/edgerouter-backup.conf
 
 sudo chown root $SSH_KEYFILE
 sudo chmod 600 $SSH_KEYFILE
