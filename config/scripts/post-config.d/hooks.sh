d7beff364fb1c7d9b73beb5e9a8f37b93873cbec
None <none@example.com>
2017-06-23 11:15:05 -0400

    Updated comments

--- config/scripts/post-config.d/hooks.sh
+++ config/scripts/post-config.d/hooks.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 source /config/user-data/edgerouter-backup.conf
 
+# This script runs at boot of the EdgeRouter
+
 # Generate symlinks to hook script(s)
 sudo ln -fs /config/user-data/hooks/* /etc/commit/post-hooks.d/
 
--- config/scripts/post-config.d/ssh_keys.sh
+++ config/scripts/post-config.d/ssh_keys.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 source /config/user-data/edgerouter-backup.conf
 
+# This script runs at boot of the EdgeRouter
+
 # Set ownership and permissions for root user so that ssh/scp don't complain.
 sudo chown root $SSH_KEYFILE
 sudo chmod 600 $SSH_KEYFILE
