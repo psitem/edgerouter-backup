dacc1cd16d3c8d9c26222690d94dccde7c31b461
None <none@example.com>
2017-06-22 21:44:20 -0400

    Updated comments.

--- config/scripts/post-config.d/ssh_keys.sh
+++ config/scripts/post-config.d/ssh_keys.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 source /config/user-data/edgerouter-backup.conf
 
-# Set ownership and permissions for root user.
+# Set ownership and permissions for root user so that ssh/scp don't complain.
 sudo chown root $SSH_KEYFILE
 sudo chmod 600 $SSH_KEYFILE
 
