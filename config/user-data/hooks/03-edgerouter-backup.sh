2e6f6425c1d16bdb5c34ad7e811fa0eac5ccd2e7
None <none@example.com>
2017-06-28 15:09:38 -0400

    Don't back up dhcpd.leases

--- config/user-data/hooks/03-edgerouter-backup.sh
+++ config/user-data/hooks/03-edgerouter-backup.sh
@@ -27,13 +27,13 @@ GIT_COMMIT_MSG="$COMMIT_CMT | by $USER | via $COMMIT_VIA | $TIME"
 # Generate temporary config files
 sudo cli-shell-api showConfig --show-active-only --show-ignore-edit --show-show-defaults > /tmp/edgerouter-backup-$FNAME_CONFIG
 sudo cli-shell-api showConfig --show-commands --show-active-only --show-ignore-edit --show-show-defaults > /tmp/edgerouter-backup-$FNAME_CLI
+sudo find /config/* | grep -v "/config/dhcpd.leases" | xargs tar cf /tmp/edgerouter-backup-$FNAME_BACKUP &> /dev/null
 
 # Push config files
 echo "edgerouter-backup: Copying backup files to $SSH_USER@$SSH_HOST:$REPO_PATH"
 sudo scp -q -i $SSH_KEYFILE -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no /tmp/edgerouter-backup-$FNAME_CONFIG $SSH_USER@$SSH_HOST:$REPO_PATH/$FNAME_CONFIG > /dev/null
 sudo scp -q -i $SSH_KEYFILE -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no /tmp/edgerouter-backup-$FNAME_CLI $SSH_USER@$SSH_HOST:$REPO_PATH/$FNAME_CLI > /dev/null
-sudo tar cf - -C / config | ssh -q -i $SSH_KEYFILE -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no $SSH_USER@$SSH_HOST "gzip -cnq > $REPO_PATH/$FNAME_BACKUP" > /dev/null
-
+sudo cat /tmp/edgerouter-backup-$FNAME_BACKUP | ssh -q -i $SSH_KEYFILE -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no $SSH_USER@$SSH_HOST "gzip -cnq9 > $REPO_PATH/$FNAME_BACKUP" > /dev/null
 
 # git commit and git push on remote host
 echo "edgerouter-backup: Triggering 'git commit'"
@@ -47,8 +47,9 @@ git push
 ENDSSH
 
 # Remove temporary files
-echo "edgerouter-backup: Removingtemporary files"
+echo "edgerouter-backup: Removing temporary files"
 sudo rm /tmp/edgerouter-backup-$FNAME_CONFIG
 sudo rm /tmp/edgerouter-backup-$FNAME_CLI
+sudo rm /tmp/edgerouter-backup-$FNAME_BACKUP
 
 echo "edgerouter-backup: Complete"
