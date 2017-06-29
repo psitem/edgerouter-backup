9ed27e3fa3fd6e684a9b5f13772acea015351533
None <none@example.com>
2017-06-29 12:10:33 -0400

    Update ownship of /config/scripts and /config/userdata to root:vyattacfg.

--- config/scripts/post-config.d/hooks.sh
+++ config/scripts/post-config.d/hooks.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 source /config/user-data/edgerouter-backup.conf
 
-# This script runs at boot of the EdgeRouter
+# Fix ownership
+sudo chown -R root:vyattacfg /config/userdata
+sudo chown -R root:vyattacfg /config/scripts
+
+# Ensure scripts are executable
+sudo chmod +x /config/user-data/hooks/*
 
 # Generate symlinks to hook script(s)
 sudo ln -fs /config/user-data/hooks/* /etc/commit/post-hooks.d/
 
-# Ensure scripts are executable
-sudo chmod +x /config/user-data/hooks/*
 exit 0
